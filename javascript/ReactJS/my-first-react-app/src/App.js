2869166636334e5dbfb7cf0a20733f4d6cff795d
whitespaces-cloud <whitespaces@wealthbeacon.ai>
2026-03-17 10:19:16 +0530

    Added content on props

--- javascript/ReactJS/my-first-react-app/src/App.js
+++ javascript/ReactJS/my-first-react-app/src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Test from './Component/Test';
 import User from './Component/User';
+import Greetings from './Component/Greetings';
 function App() {
   return (
-    <User/>
+    <>
+      <User/>
+      <Greetings name="Jacob Dennis" age={25} />
+    </>
   );
 }
 
