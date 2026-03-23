73941d18d8f9915cde5bb154f356c2937e4d6703
tecsxpert <dev@tecsxpert.xyz>
2026-03-23 10:05:26 +0530

    Added React Router dome

---
+++ javascript/ReactJS/formsystem/src/components/ForgotPassword.jsx
@@ -0,0 +1,7 @@
+function ForgotPassword(){
+ return (
+        <h1>This is Forgot Password</h1>
+    )
+}
+
+export default ForgotPassword;
---
+++ javascript/ReactJS/formsystem/src/components/ResetPassword.jsx
@@ -0,0 +1,7 @@
+function ResetPassword(){
+ return (
+        <h1>This is Reset Password</h1>
+    )
+}
+
+export default ResetPassword;
